163986a55d5a06c36c244d05b0819a226fca89f2
jessecurry <jesse@jessecurry.net>
2011-09-22 16:32:34 -0400

    Initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weatherman::Application.config.secret_token = '9c13d19312680894bd65575f848469a3075d351e30b2af3a5717c4bc1a9541cb4143018345b5577e2ff23ce82564df0d7c7398ee5aac46b4471cacef82bc5861'
